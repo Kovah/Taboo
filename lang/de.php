eca23a3c11f18143375b6d76665b9180e1adb3c2
Kovah <mail@kovah.de>
2016-08-19 21:07:11 +0200

    Adds first working version of the game

---
+++ lang/de.php
@@ -0,0 +1,45 @@
+<?php
+$lang = [
+    'app.name' => 'KVH Tabu',
+    'app.description' => 'KVH Tabu ist ein kleines Tabu-ähnliches Spiel',
+    'app.how-to' => '<h3>So wird gespielt</h3>
+      <ol>
+        <li>Gib deinen Namen ein und wähle eine Kategorie</li>
+        <li>Start the game</li>
+        <li>Du hast 120 Sekunden, um deinen Freunden das angezeigte Wort zu erklären. Verwende aber nicht das Wort oder Teile des Wortes.</li>
+        <li>Klicke auf den grünen Button, wenn deine Freunde das Wort erraten haben. Klicke Rot, wenn du das Wort überspringen möchtest.</li>
+        <li>Die Ergebnisse werden in deinem Browser gespeichert</li>
+      </ol>',
+
+    'category.all'  => 'Alle Kategorie',
+    'category.animals'  => 'Tiere',
+    'category.people'  => 'Berühmte Personen',
+    'category.cars'  => 'Fahrzeuge',
+    'category.food'  => 'Essen & Trinken',
+    'category.things'  => 'Dinge & Geräte',
+    'category.city-country'  => 'Städte und Länder',
+    'category.movies'  => 'Filme',
+    'category.literature'  => 'Literatur',
+    'category.sports'  => 'Sport',
+
+    'game.start' => 'Starten!',
+    'game.get-ready' => 'Mach dich bereit!',
+    'game.time-remaining' => 'Verbleibende Zeit:',
+    'game.exit' => 'Beenden',
+    'game.over' => 'Spiel zu Ende!',
+    'game.error' => 'Aufgrund eines Fehlers konnte das Spiel nicht geladen werden.',
+
+    'app.enter-name' => 'Gib deinen Namen ein',
+    'app.back-to-menu' => 'Zurück zum Menu',
+    'app.show-results' => 'Zeige die Ergebnisse',
+    'app.show-highscores' => 'Zeige die Highscores',
+
+    'results' => 'Deine Ergebnisse',
+    'results.for' => 'Ergebnisse für',
+    'results.correct' => 'Geratene Wörter',
+    'results.skipped' => 'Übersprungene Wörter',
+
+    'highscores' => 'Highscores',
+    'highscores.unavailable' => 'Highscores können leider nicht in deinem Browser gespeichert werden.',
+    'highscores.reset' => 'Highscores zurücksetzen',
+];
---
+++ lang/en.php
@@ -0,0 +1,45 @@
+<?php
+$lang = [
+    'app.name' => 'KVH Tabu',
+    'app.description' => 'KVH Tabu is a small, Tabu-like game.',
+    'app.how-to' => '<h3>How to play</h3>
+      <ol>
+        <li>Enter your name and choose a category</li>
+        <li>Start the game</li>
+        <li>You have 120 seconds to explain the shown word to your friends <b>without</b> using the word or parts of it</li>
+        <li>Tap on the check mark if your friends guessed the word. Tap the cross to pick the next word.</li>
+        <li>Scores will be saved in your browser</li>
+      </ol>',
+
+    'category.all'  => 'All Categories',
+    'category.animals'  => 'Animals',
+    'category.people'  => 'Famous People',
+    'category.cars'  => 'Vehicles',
+    'category.food'  => 'Food and Drinks',
+    'category.things'  => 'Things and Devices',
+    'category.city-country'  => 'Cities and Countries',
+    'category.movies'  => 'Movies',
+    'category.literature'  => 'Literature',
+    'category.sports'  => 'Sports',
+
+    'game.start' => 'Start!',
+    'game.get-ready' => 'Get ready!',
+    'game.time-remaining' => 'Time remaining:',
+    'game.exit' => 'Exit',
+    'game.over' => 'Game Over!',
+    'game.error' => 'There was an error loading the game.',
+
+    'app.enter-name' => 'Enter your Name',
+    'app.back-to-menu' => 'Back to Menu',
+    'app.show-results' => 'Show my Results',
+    'app.show-highscores' => 'Show the Highscores',
+
+    'results' => 'Your Results',
+    'results.for' => 'Results for',
+    'results.correct' => 'Correct Words',
+    'results.skipped' => 'Skipped Words',
+
+    'highscores' => 'Highscores',
+    'highscores.unavailable' => 'Highscores cannot be saved in your Browser.',
+    'highscores.reset' => 'Reset Highscores',
+];
