fea315ff47766ca2a81fa65a06891046c369a095
Kovah <mail@kovah.de>
2018-08-05 00:27:54 +0200

    Display the correct category name in highscores

--- src/components/Highscores/Highscore/Highscore.vue
+++ src/components/Highscores/Highscore/Highscore.vue
@@ -2,15 +2,22 @@
   <tr class="highscore">
     <td class="highscore__index">{{ highscoreIndex + 1 }}</td>
     <td class="highscore__name">{{ highscore.name }}</td>
-    <td class="highscore__category">{{ highscore.category }}</td>
+    <td class="highscore__category">{{ categoryName(highscore.category) }}</td>
     <td class="highscore__score-success">{{ highscore.score.success }}</td>
     <td class="highscore__score-fail">{{ highscore.score.fail }}</td>
   </tr>
 </template>
 
 <script>
+  import GameData from '../../GameData';
+
   export default {
     name: 'Highscore',
-    props: ['highscore', 'highscoreIndex']
+    props: ['highscore', 'highscoreIndex'],
+    methods: {
+      categoryName (category) {
+        return GameData.categories[category].text;
+      }
+    }
   };
 </script>
