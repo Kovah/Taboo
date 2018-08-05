11c7fb81073c911a0c594449a3a0d88eb454c82f
Kovah <mail@kovah.de>
2018-08-05 13:12:28 +0200

    Display the correct round length in the howto

--- src/components/Menu/HowTo/HowTo.vue
+++ src/components/Menu/HowTo/HowTo.vue
@@ -4,7 +4,7 @@
     <ol>
       <li>Gib deinen Namen ein und wähle eine Kategorie.</li>
       <li>Starte das Spiel.</li>
-      <li>Du hast 180 Sekunden, um deinen Freunden das angezeigte Wort zu erklären. Du darfst zur Erklärung nicht die
+      <li>Du hast {{ roundLength }} Sekunden, um deinen Freunden das angezeigte Wort zu erklären. Du darfst zur Erklärung nicht die
         Wörter aus dem roten Bereich nutzen!
       </li>
       <li>Klicke auf den grünen Button, wenn deine Freunde das Wort erraten haben. Klicke Rot, wenn du das Wort
@@ -17,6 +17,11 @@
 
 <script>
   export default {
-    name: 'HowTo'
+    name: 'HowTo',
+    computed: {
+      roundLength() {
+        return this.$store.state.timerDefault
+      }
+    }
   };
 </script>
