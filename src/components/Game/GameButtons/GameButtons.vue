f3c82bc1d17462259396cfb5493431d020566c4e
Kovah <mail@kovah.de>
2020-08-20 17:46:19 +0200

    Allow player to use left/right arrow keys to toggle fail/success buttons

--- src/components/Game/GameButtons/GameButtons.vue
+++ src/components/Game/GameButtons/GameButtons.vue
@@ -2,12 +2,12 @@
   <div class="game-buttons">
 
     <button type="button" class="btn btn--lg btn--fail game-button" id="game-fail"
-      v-on:click="markFail" v-bind:disabled="!gameStarted">
+      ref="failBtn" v-on:click="markFail" v-bind:disabled="!gameStarted">
       <img src="@/assets/cross.svg" alt="&cross;">
     </button>
 
     <button type="button" class="btn btn--lg btn--success game-button" id="game-success"
-      v-on:click="markSuccess" v-bind:disabled="!gameStarted">
+      ref="successBtn" v-on:click="markSuccess" v-bind:disabled="!gameStarted">
       <img src="@/assets/check.svg" alt="&check;">
     </button>
 
@@ -22,12 +22,39 @@
         return this.$store.state.gameStarted;
       }
     },
+    mounted () {
+      this.$store.subscribe((mutation, state) => {
+        if (mutation.type === 'startGame') {
+          window.addEventListener('keydown', this.keyboardListener);
+        }
+
+        if (mutation.type === 'stopGame') {
+          window.removeEventListener('keydown', this.keyboardListener);
+        }
+      });
+    },
     methods: {
       markSuccess () {
         this.$store.commit('commitSuccess');
       },
       markFail () {
         this.$store.commit('commitFail');
+      },
+      pretendButtonClick (buttonReference) {
+        this.$refs[buttonReference].classList.add('active');
+
+        setTimeout(() => {
+          this.$refs[buttonReference].classList.remove('active');
+        }, 150);
+      },
+      keyboardListener (event) {
+        if (event.key === 'ArrowLeft') {
+          this.pretendButtonClick('failBtn');
+          this.markFail();
+        } else if (event.key === 'ArrowRight') {
+          this.pretendButtonClick('successBtn');
+          this.markSuccess();
+        }
       }
     }
   };
